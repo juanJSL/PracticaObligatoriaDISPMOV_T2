336d22aa58b1e7866955b728ceeae6ea17720466
Unknown <jjolula88@gmail.com>
2017-12-11 14:49:38 +0100

    Funcionalidad a medias
    Edad

--- app/src/main/java/com/example/jj/practicaobligatoriadispmov_t2/Registro.java
+++ app/src/main/java/com/example/jj/practicaobligatoriadispmov_t2/Registro.java
@@ -1,21 +1,22 @@
 package com.example.jj.practicaobligatoriadispmov_t2;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Registro extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     private static int anyo, mes, dia;
     private static EditText fecha;
+    private static EditText nombre;
+    private static EditText mail;
     private static DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
@@ -23,7 +24,9 @@ public class Registro extends AppCompatActivity implements DatePickerDialog.OnDa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        fecha = findViewById(R.id.fechaET);
+        fecha = (EditText) findViewById(R.id.fechaET);
+        nombre = (EditText) findViewById(R.id.nombreET);
+        mail = (EditText) findViewById(R.id.mailET);
 
         dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -57,4 +60,44 @@ public class Registro extends AppCompatActivity implements DatePickerDialog.OnDa
         dia=dia;
         fecha.setText(dia+"-"+mes+"-"+anyo);
     }
+
+    public void comprobarEdad(View v) {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-MM-yyyy");
+        Date fNac = null;
+        Date fActual = null;
+        int edad;
+        try {
+            fNac = formatoFecha.parse(fecha.getText().toString());
+            fActual = new Date();
+            edad = (fActual)-()
+            nombre.setText(""+ fNac.getTime());
+        } catch (ParseException e) {
+            nombre.setText("Ex");
+        }
+
+
+
+    }
+
+/*
+    //Guardar estado de la activity
+
+    String str = nombre.getText().toString();
+    @Override
+    protected void onSaveInstanceState(Bundle guardarEstado) {
+        super.onSaveInstanceState(guardarEstado);
+        guardarEstado.putString("nombre", nombre.getText().toString());
+        guardarEstado.putString("mail", mail.getText().toString());
+        guardarEstado.putString("fecha", fecha.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle recEstado) {
+        super.onRestoreInstanceState(recEstado);
+        nombre.setText(recEstado.getString("nombre"));
+        mail.setText(recEstado.getString("mail"));
+        fecha.setText(recEstado.getString("fecha"));
+    }
+ */
+
 }
